d1bf2f5dfbab8886688c023c7eabb9327283d20b
klfischer89 <fischer-k4@web.de>
2025-12-05 14:26:39 +0100

    finished model to predict prices of cars, considering kilometer and yearOfRegestration

--- project_cars.py
+++ project_cars.py
@@ -1,6 +1,8 @@
 import numpy as np
 import seaborn as sns
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 df = pd.read_csv("./data/autos.csv.bz2", encoding = "iso-8859-1")
 
@@ -9,3 +11,21 @@ df = df[df["vehicleType"] == "kleinwagen"]
 df = df[df["notRepairedDamage"] == "nein"]
 
 df.dropna(inplace = True)
+
+X = df[["kilometer", "yearOfRegistration"]]
+y = df["price"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.75)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+print(model.score(X_train, y_train))
+print(model.score(X_test, y_test))
+
+X_pred = np.array([
+    [150000, 2000],
+    [100000, 1998]
+])
+
+print(model.predict(X_pred))
