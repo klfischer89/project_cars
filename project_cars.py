662f000de8e607ca8af77dfc712f3a6a759a0bbb
klfischer89 <fischer-k4@web.de>
2025-12-05 14:52:17 +0100

    using one hot encoding to let the model work with brands in addition to kilometer and yearOfRegestration

--- project_cars.py
+++ project_cars.py
@@ -3,6 +3,8 @@ import seaborn as sns
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
 
 df = pd.read_csv("./data/autos.csv.bz2", encoding = "iso-8859-1")
 
@@ -12,10 +14,18 @@ df = df[df["notRepairedDamage"] == "nein"]
 
 df.dropna(inplace = True)
 
-X = df[["kilometer", "yearOfRegistration"]]
+X = df[["kilometer", "yearOfRegistration", "brand"]]
 y = df["price"]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.75)
+cf = ColumnTransformer([
+    ("brand", OneHotEncoder(), ["brand"])
+], remainder = "passthrough")
+ 
+cf.fit(X)
+
+X_transformed = cf.transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X_transformed, y, train_size = 0.75)
 
 model = LinearRegression()
 model.fit(X_train, y_train)
@@ -23,9 +33,3 @@ model.fit(X_train, y_train)
 print(model.score(X_train, y_train))
 print(model.score(X_test, y_test))
 
-X_pred = np.array([
-    [150000, 2000],
-    [100000, 1998]
-])
-
-print(model.predict(X_pred))
